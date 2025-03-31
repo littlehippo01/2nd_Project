ab895789b5199b75e98742ae61b98276a9d683ba
쑤아 <63803295+kimsua88@users.noreply.github.com>
2025-03-31 17:55:22 +0900

    새로 작업한 내용
    info 전문 첫 개발 이루어짐 
    디자인 아직 안됨

--- Project_Forder/team2_2nd_project_tabbar_app/lib/Information/info_1.dart
+++ Project_Forder/team2_2nd_project_tabbar_app/lib/Information/info_1.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:team2_2nd_project_tabbar_app/Information/info_2.dart';
+import 'package:team2_2nd_project_tabbar_app/Information/info_3.dart';
 
 class Info1 extends StatefulWidget {
   const Info1({super.key});
@@ -9,7 +11,66 @@ class Info1 extends StatefulWidget {
 
 class _Info1State extends State<Info1> {
   @override
-  Widget build(BuildContext context) {
-    return const Placeholder();
+    Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey,
+      appBar: AppBar(
+        title: Text("보더콜리를 알아보자"),
+        backgroundColor: Colors.black,
+        foregroundColor: Colors.white,
+      ),
+
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            SizedBox(
+              height: 400,
+              child: Container(
+                child: Image.asset('images/1234.png',
+                width: 250,
+                height: 350,
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(30.0),
+              child: Text("📖 보더콜리란?",
+              style: TextStyle(
+                fontSize: 30,
+                fontWeight: FontWeight.bold,
+                backgroundColor: Colors.white,
+              ),),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: ElevatedButton(onPressed:() {
+                     Navigator.push(context,MaterialPageRoute(builder: (context){
+            return Info2();
+          }));
+                  } , 
+                  child: Text('설명보러가기'),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: ElevatedButton(onPressed:() {
+                     Navigator.push(context,MaterialPageRoute(builder: (context){
+            return Info3();
+          }));
+                  } , 
+                  child: Text('장단점'),
+                  ),
+                )
+              ],
+            ),
+
+          ],
+        ),
+      ),
+    );
   }
 }
